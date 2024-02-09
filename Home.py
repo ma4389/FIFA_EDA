3742d8f95ab8b0007dc5eb4e5b0fb92781624b07
ma4389 <149170027+ma4389@users.noreply.github.com>
2024-02-09 14:23:07 +0200

    Add files via upload

---
+++ Home.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import pandas as pd 
+st.title('FIfa')
+st.markdown("<h1 style = 'text-align: center ; color #198542 ;'> FIFA 19 </h1> ")
+st.image('franchise-hero-tertiary-fifa19-home-update-key-art-xs.jpg',  use_column_width = 'True')
+
+st.link_button('fifa_Kaggle_DataSet' , "https://www.kaggle.com/thec03u5/fifa-18-demo-player-dataset")
+
+
